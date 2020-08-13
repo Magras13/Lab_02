26528aa499a940b293cb98ac961ddc8002091e9d
Magras13 <37419938+Magras13@users.noreply.github.com>
2020-08-13 20:00:21 +0300

    Add files via upload

--- Rubles.cpp
+++ Rubles.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int rub_5, rub_2, rub_1, A;
+    int rub_5, rub_2, rub_1, res_5,res_2,res_1, A;
     bool result = false;
 
     cout << "Input 5 rub quantity\n";
@@ -23,13 +23,16 @@ int main() {
             {
                 if (i * 5 + j * 2 + k == A)
                 {
+                    res_5 = i;
+                    res_2 = j;
+                    rex_3 = k;
                     result = true;
                 }
             }
         }
     }
     if (result == true)
-        cout << "YES";
+        cout << "YES you need "<< res_5 << " five rub coin(s) and " << res_2 << " two rub coin(s) and " << res_1 << " one rub coin(s)";
     else
         cout << "NO";
 }
